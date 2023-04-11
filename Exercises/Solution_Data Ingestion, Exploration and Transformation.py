e35fb3fbad849316ec20913cba6796913ab03c3a
Avinash Sooriyarachchi <98932637+avisoori-databricks@users.noreply.github.com>
2023-04-11 09:36:08 -0400

    Update Solution_Data Ingestion, Exploration and Transformation.py

--- Exercises/Solution_Data Ingestion, Exploration and Transformation.py
+++ Exercises/Solution_Data Ingestion, Exploration and Transformation.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-#Uncomment the following line, click on the button to install bamboolib. Run the couple of cells thus created to install and import Bamboolib
+#Uncomment the following line, click on the button to install bamboolib. Run the couple of cells thus created to install and import Bamboolib (the latter is optional)
 import pandas as pd
 
 # COMMAND ----------
